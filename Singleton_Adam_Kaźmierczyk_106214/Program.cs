e1fcdd3c9da5b4061a5ed26db685e83f73def1ed
Adam Kazmierczyk 106214 <73425590+adamus9050@users.noreply.github.com>
2023-02-16 11:21:13 +0100

    Dodanie zadnaia

---
+++ Singleton_Adam_Kaźmierczyk_106214/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+public class Singleton
+{
+    private static Singleton instance = null;
+    private static readonly object _lock = new object(); 
+    private Singleton() { }
+
+    public static Singleton Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                lock (_lock)  //jesli lock active to znaczy, że jakiś wątek działa na tej instancji i inny nie może wejść.                {
+                    if(instance==null)
+                    {
+                        instance = new Singleton();
+                    }
+
+                }
+                
+            }
+            return instance;
+        }
+    }
+}
